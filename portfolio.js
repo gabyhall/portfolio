6193819e1bc5059e131ed8055d62baac3f9992b9
gabyhall <85550804+gabyhall@users.noreply.github.com>
2021-06-17 09:59:22 +0100

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,14 @@
+let myIndex = 0;
+carousel();
+
+function carousel() {
+  let i;
+  let x = document.getElementsByClassName("media");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 4000); // Change image every 4 seconds
+}
